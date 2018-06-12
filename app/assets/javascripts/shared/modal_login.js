497b015513a3072f49735b86b97aef8e834123a9
Felipe Felicio <felipefelicio@Felipes-MacBook-Air.local>
2018-06-12 16:33:33 -0300

    update layout/home

--- app/assets/javascripts/shared/modal_login.js
+++ app/assets/javascripts/shared/modal_login.js
@@ -1,12 +1,12 @@
 $(document).on("turbolinks:load", function() {
-  $('.btn-cadastrar').click(function(){
+  $('.btn-cadastrar, .btn-signup').click(function(){
     $('#form-login').fadeOut();
     $('#form-signup').delay(800).fadeIn();
     $('.btn-login').removeClass('active');
     $('.btn-signup').addClass('active');
 
   })
-  $('.btn-cadastrado').click(function(){
+  $('.btn-cadastrado, .btn-login').click(function(){
     $('#form-signup').fadeOut();
     $('#form-login').delay(800).fadeIn();
     $('.btn-signup').removeClass('active');
